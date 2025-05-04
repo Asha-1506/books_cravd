10ec548ecf1241f539c5ac03f12f73ef708b9035
Asha-1506 <asha.vasudeva@gmail.com>
2025-05-04 17:37:00 +0530

    Update Render configuration and build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,13 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 bundle install
+
+# Set up database
+bundle exec rake db:schema:load
+bundle exec rake db:migrate
+
+# Precompile assets
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
