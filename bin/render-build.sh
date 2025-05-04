de455f446a9b653d227979539468d67774dcedb5
Asha-1506 <asha.vasudeva@gmail.com>
2025-05-04 23:02:19 +0530

    Update Ruby version specification for Render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,15 +2,6 @@
 # exit on error
 set -o errexit
 
-# Install specific Ruby version
-curl -sSL https://get.rvm.io | bash -s stable
-source /etc/profile.d/rvm.sh
-rvm install 3.2.2
-rvm use 3.2.2 --default
-
-# Install bundler
-gem install bundler -v 2.4.22
-
 # Install dependencies
 bundle install
 
