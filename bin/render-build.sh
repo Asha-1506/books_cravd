e041b0d7b43c2e3ad8e2b96dd0f507eef0e3607a
Asha-1506 <asha.vasudeva@gmail.com>
2025-05-04 22:39:07 +0530

    Simplify build configuration

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,21 +2,16 @@
 # exit on error
 set -o errexit
 
-# Clear bundler environment
-unset BUNDLE_PATH
-unset BUNDLE_BIN
-
-# Install latest bundler
-gem update --system
-gem install bundler -v '2.4.22'
+export BUNDLE_DEPLOYMENT=false
+export BUNDLE_PATH=vendor/bundle
+export BUNDLE_WITHOUT="development:test"
 
 # Install gems
-bundle config set --local without 'development test'
-bundle install
+bundle check || bundle install
 
 # Clean and precompile assets
-bundle exec rake assets:clean
-bundle exec rake assets:precompile
+RAILS_ENV=production bundle exec rake assets:clean
+RAILS_ENV=production bundle exec rake assets:precompile
 
 # Set up database
-bundle exec rake db:migrate
+RAILS_ENV=production bundle exec rake db:migrate
